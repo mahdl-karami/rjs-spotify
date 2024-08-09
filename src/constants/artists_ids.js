a0fe82a2735ec6cb0ba664536f8265597842e024
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-09 15:20:24 +0330

    add scroll to sidebar

--- src/constants/artists_ids.js
+++ src/constants/artists_ids.js
@@ -1,13 +1,28 @@
-const artists_ids = `
-3JS9sHeI06RtolBR5s5O0L,
-1wJm5nKEUlhdV48hMMwzAm,
-1UjD9VWeqDDlDSvNlnFTdl,
-6fOMl44jA4Sp5b9PpYCkzz,
-3QNGoF6VzVNnkpjJDT3NHq,
-5CEosSs2y4M9THNGI6mej8,
-2YZyLoL8N0Wb9xBt1NhZWg,
-7dGJo4pcD2V6oG8kP0tJRR,
-246dkjvS1zLTtiykXe5h60
-`;
+const artists_ids = [
+  "3JS9sHeI06RtolBR5s5O0L",
+  "1wJm5nKEUlhdV48hMMwzAm",
+  "1UjD9VWeqDDlDSvNlnFTdl",
+  "6fOMl44jA4Sp5b9PpYCkzz",
+  "3QNGoF6VzVNnkpjJDT3NHq",
+  "5CEosSs2y4M9THNGI6mej8",
+  "2YZyLoL8N0Wb9xBt1NhZWg",
+  "7dGJo4pcD2V6oG8kP0tJRR",
+  "246dkjvS1zLTtiykXe5h60",
+  "3VzZOmXc8pZRfNxoiliE1A",
+  "0bRZzB4e6Yj3B64Rcbs1vd",
+  "0u4qrFczDmAsJesHPgbnru",
+  "7Hj58arwOvp6exTny9r5Ie",
+  "2X90kCLyxyPeJ5nynJGbvT",
+  "5vVveQB8n4kETe67waTS3t",
+  "5zixe6AbgXPqt4c1uSl94L",
+  "45YMrIBH74j8e2wNlRSSdK",
+  "5er043agmHdVZkWTxL0Lpk",
+  "1URnnhqYAYcrqrcwql10ft",
+  "0Y5tJX1MQlPlqiwlOH1tJY",
+  "1yPzb9mqugowOfUs2vIOgL",
+  "0iEtIxbK0KxaSlF7G42ZOp",
+  "6jj9lOTeZC28LkPoXK9hiT",
+  "7hJcb9fa4alzcOq3EaNPoG"
+]
 
-export default artists_ids.replace(/\s/g, "");
+export default artists_ids.join(",");
