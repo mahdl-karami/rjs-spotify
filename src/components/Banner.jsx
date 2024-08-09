34ef198e44750e0d1e20c6172723790469ca9aed
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-09 10:27:25 +0330

    handle error and loading in banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,65 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { MdVerified } from "react-icons/md";
+import ARTIST from "../services/artist";
 
-function Banner({ artist }) {
+function Banner({ artistId }) {
+
+  const [artist, setArtist] = useState([]);
+  const [loading, setLoading] = useState(!artist.length);
+  const [err, setErr] = useState(false);
+
+  useEffect(() => {
+    if (artist.length) {
+      console.log("artist is ready");
+    } else {
+      setLoading(true);
+      const { URL, DATA } = ARTIST(artistId);
+      fetch(URL, { ...DATA })
+        .then((res) => res.json())
+        .then((json) => {
+          setArtist(json);
+          setLoading(false);
+        })
+        .catch((err) => setErr(err));
+    }
+  }, [artistId]);
+
   return (
     <div className="banner">
-      <div className="banner-image">
-        <img src={artist?.images[0].url} alt="" />
-      </div>
-      <div className="banner-text">
-        <span>
-          <MdVerified /> Verified Artist
-        </span>
-        <h1>{artist?.name}</h1>
-        <p>Folowers : {artist?.followers.total}</p>
-      </div>
+      {err && (
+        <>
+          <div className="banner-image banner-loading"></div>
+          <div className="banner-text">
+            <span>Oh Nooo !</span>
+            <h1>{"error : " + err}</h1>
+            <p>Please report to me : mo.mahdi.ka@gmail.com</p>
+          </div>
+        </>
+      )}
+      {loading && (
+        <>
+          <div className="banner-image banner-loading"></div>
+          <div className="banner-text">
+            <span>Pleas Wait ...</span>
+            <h1>Loading Data ...</h1>
+            <p>Folowers : ???</p>
+          </div>
+        </>
+      )}
+      {!loading && !err && (
+        <>
+          <div className="banner-image">
+            <img src={artist?.images[0].url} alt="" />
+          </div>
+          <div className="banner-text">
+            <span>
+              <MdVerified /> Verified Artist
+            </span>
+            <h1>{artist?.name}</h1>
+            <p>Folowers : {artist?.followers.total}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/routes/Artist.jsx
+++ src/routes/Artist.jsx
@@ -1,24 +1,13 @@
 import { useParams } from "react-router-dom";
-import ARTIST from "../services/artist";
-import { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import TracksTable from "../components/TracksTable";
 
 function Artist() {
   const artistId = useParams().artistId;
-  const [artist, setArtist] = useState(undefined);
-
-  useEffect(() => {
-    const { URL, DATA } = ARTIST(artistId);
-    fetch(URL, { ...DATA })
-      .then((res) => res.json())
-      .then((json) => setArtist(json));
-  }, [artistId]);
-
   return (
     <div className="artist-profile">
-      <Banner artist={artist} />
-      <TracksTable  artistId={artistId}/>
+      <Banner artistId={artistId} />
+      <TracksTable artistId={artistId} />
     </div>
   );
 }
