26a61c0c77300c4d0683e1aff8ef347102a92ec0
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-09 17:09:50 +0330

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+//? import icons
+import { FaSpotify } from "react-icons/fa";
+
 function App() {
   return (
-    <>
-      <h1>Spotify</h1>
-      <button onClick={() => console.log(loading)}>give me log</button>
-    </>
+    <div className="home">
+      <span className="home-logo">
+        <FaSpotify />
+        <h1> Spotify </h1>
+      </span>
+      <div className="home-text">
+        <p>
+          UI Template website :{" "}
+          <a href="https://open.spotify.com/" target="_blank">
+            spotify
+          </a>
+        </p>
+        <p>
+          API services :{" "}
+          <a href="https://developer.spotify.com/" target="_blank">
+            developer spotify
+          </a>
+        </p>
+        <p>
+          Developer :{" "}
+          <a href="https://github.com/mahdl-karami" target="_blank">
+            Mahdl-Karami
+          </a>
+        </p>
+      </div>
+    </div>
   );
 }
 
