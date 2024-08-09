2239941c9a4373571ac581f0f70415614382ce7f
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-09 10:44:22 +0330

    handle error and loading in trackstable

--- src/components/TracksTable.jsx
+++ src/components/TracksTable.jsx
@@ -3,30 +3,39 @@ import TRACKS from "../services/topTracks";
 
 function TracksTable({ artistId }) {
   const [topTracks, setTopTracks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [err, setErr] = useState(false);
   useEffect(() => {
+    setLoading(true);
     const { URL, DATA } = TRACKS(artistId);
     setTopTracks([]);
     fetch(URL, { ...DATA })
       .then((res) => res.json())
       .then((json) => {
         setTopTracks(json?.tracks);
-        console.log(topTracks);
+        setLoading(false);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => setErr(err));
   }, [artistId]);
   return (
-    <div className="tracks-table">
-      {topTracks?.map((track, index) => (
-        <div>
-          <p>{index + 1}</p>
-          <div>
-            <img src={track.album.images[2].url} alt="" />
-            <h3>{track.name}</h3>
-          </div>
-          <p>5:55</p>
+    <>
+      {err && <p>Ops, Error : {err}</p>}
+      {loading && <></>}
+      {!loading && !err && (
+        <div className="tracks-table">
+          {topTracks?.map((track, index) => (
+            <div key={index}>
+              <p>{index + 1}</p>
+              <div>
+                <img src={track.album.images[2].url} alt="" />
+                <h3>{track.name}</h3>
+              </div>
+              <p>5:55</p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 }
 
